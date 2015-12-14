1b126c3d1dca4d4295feb9fa7207f1a111084ff7
JbRezidu <duriez.jeanbaptiste@gmail.com>
2015-12-14 18:53:05 +0100

    Fin de l'exercice 3

--- OptiCombinatoire/optiCombinatoire.c
+++ OptiCombinatoire/optiCombinatoire.c
@@ -224,6 +224,74 @@ void resetSolution(InstanceSacADosSolution *S)
     }
 };
 
+float max(float a, float b) {
+    if (a >= b) {
+        return a;
+    }
+    return b;
+};
+
+void solutionUsingDynamicProgrammation (InstanceSacADosSolution *S) {
+    //Initialisation de la matrice
+    //les colonnes correspondent aux nombres d'objets à mettre
+    //les lignes la capacité du sac
+    float tabDynamicProgra[S->instance.capaciteMax + 1][S->instance.nbrObjet + 1];
+    int i, j;
+    for (i = 0; i <= S->instance.capaciteMax; i++) {
+        tabDynamicProgra[i][0] = 0;
+    }
+    //Construction de la matrice
+    for (i = 0; i <= S->instance.capaciteMax; i++) {
+        for (j = 1; j <= S->instance.nbrObjet; j++) {
+            if (S->instance.objets[j - 1].poids > i) {
+                tabDynamicProgra[i][j] = tabDynamicProgra[i][j-1];
+            }
+            else {
+                tabDynamicProgra[i][j] = max(tabDynamicProgra[i][j-1], (float)S->instance.objets[j - 1].profit + tabDynamicProgra[i - S->instance.objets[j - 1].poids][j - 1]);
+            }
+        }
+    }
+    //Récupération des résultats
+    i = S->instance.capaciteMax;
+    j = S->instance.nbrObjet;
+    while (tabDynamicProgra[i][j] == tabDynamicProgra[i - 1][j]) {
+        i--;
+    }
+    while (i > 0) {
+        while (j > 0 && tabDynamicProgra[i][j] == tabDynamicProgra[i][j - 1]) {
+            j--;
+        }
+        if (j != 0) {
+            i -= S->instance.objets[j - 1].poids;
+            if (j > 0) {
+                S->solution.objetsSolution[j - 1].present = 1;
+            }
+            j--;
+        }
+    }
+    //Affichage des résultats
+    for (i = 0; i < S->instance.nbrObjet; i++) {
+        if (S->solution.objetsSolution[i].present == 0)
+        {
+            printf("L'objet numero %d n'est pas ajoute\n", i);
+        }
+        else
+        {
+            printf("L'objet numero %d est ajoute ", i);
+            if (S->solution.objetsSolution[i].present == 1)
+            {
+                printf("entierement\n");
+            }
+            else
+            {
+                printf("partiellement (a %.2f%%)\n", S->solution.objetsSolution[i].present * 100);
+            }
+        }
+    }
+    printf("La valeur de la solution optimale avec l'algo de programmation dynamique est : %.2f\n", tabDynamicProgra[S->instance.capaciteMax][S->instance.nbrObjet]);
+
+};
+
 int main()
 {
     char nomFichier[50];
@@ -239,7 +307,8 @@ int main()
         printf("\n\n------------------------------------------\n");
         printf("Que souhaitez-vous faire ? \n");
         printf("1 - Resoudre le probleme du sac a dos en utilisant l'heuristique gloutonne (relaxation continue)\n");
-        printf("2 - Resoudre le probleme du sav a dos en utilisant l'heuristique gloutonne (solution realisable)\n");
+        printf("2 - Resoudre le probleme du sac a dos en utilisant l'heuristique gloutonne (solution realisable)\n");
+        printf("3 - Resoudre le probleme du sac a dos en utilisant un algorithme de programmation dynamique\n");
         printf("0 - Sortir du programme ? \n");
         scanf("%d", &choix);
         printf("\n\n");
@@ -251,9 +320,14 @@ int main()
             solutionUsingAlgoGlouton(&S, 1);
             break;
         case 2:
-            printf("Resultat du probleme de sac a dos avec l'istique gloutonne avec une solution realisable :\n");
+            printf("Resultat du probleme de sac a dos avec l'heuristique gloutonne avec une solution realisable :\n");
             resetSolution(&S);
             solutionUsingAlgoGlouton(&S, 0);
+            break;
+        case 3:
+            printf("Resultat du probleme de sac a dos avec un algorithme de programmation dynamique :\n");
+            resetSolution(&S);
+            solutionUsingDynamicProgrammation(&S);
             break;
         default:
             printf("Au revoir\n");
